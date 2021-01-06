9ec8b6c3cf805b3f156ccf4fef8364e7d43fd71d
Arcadie Condrat <arcadie.condrat@gmail.com>
2021-01-06 15:47:53 +0200

    small load testing script

---
+++ load/load.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"log"
+	"sync"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/hazelcast/hazelcast-go-client"
+	"github.com/hazelcast/hazelcast-go-client/core"
+)
+
+func main() {
+	// Connect
+	clientConfig := hazelcast.NewConfig()
+	clientConfig.NetworkConfig().AddAddress("hazelcast:5701")
+	client, _ := hazelcast.NewClientWithConfig(clientConfig)
+
+	// The map is stored on the server but we can access it from the client
+	mapName := "default"
+	mp, _ := client.GetMap(mapName)
+
+	// clear the map
+	mp.Clear()
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < 3; i++ {
+		wg.Add(1)
+		go load(mp, 10000000, &wg)
+	}
+
+	wg.Add(1)
+	go printSize(mp, &wg)
+
+	wg.Wait()
+
+	// Disconnect
+	client.Shutdown()
+}
+
+func printSize(mp core.Map, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		size, _ := mp.Size()
+		log.Printf("Map '%v' Size %v\n", mp.Name(), size)
+		time.Sleep(time.Second)
+	}
+}
+
+func load(mp core.Map, items int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 0; i < items; i++ {
+		rnd := uuid.New().String()
+		mp.SetWithTTL(rnd, rnd, time.Duration(3600)*time.Second)
+		mp.Get(rnd)
+
+	}
+}
