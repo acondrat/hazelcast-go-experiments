275486bc9b87b5ce6e8eef89c82472ecb5cac40e
Arcadie Condrat <arcadie.condrat@gmail.com>
2021-01-11 12:08:39 +0200

    wip

--- load/load.go
+++ load/load.go
@@ -26,15 +26,16 @@ func main() {
 	client, _ := hazelcast.NewClientWithConfig(config)
 
 	// The map is stored on the server but we can access it from the client
-	mapName := "default"
-	mp, _ := client.GetMap(mapName)
+	// mapName := "default"
+	// mp, _ := client.GetMap(mapName)
 
 	for {
-		size, _ := mp.Size()
-		log.Printf("Map '%v' Size %v\n", mp.Name(), size)
+		// size, _ := mp.Size()
+		// log.Printf("Map '%v' Size %v\n", mp.Name(), size)
 
-		rnd := uuid.New().String()
-		mp.SetWithTTL(rnd, rnd, time.Duration(3600)*time.Second)
+		// rnd := uuid.New().String()
+		// mp.SetWithTTL(rnd, rnd, time.Duration(3600)*time.Second)
+		log.Printf("waiting...")
 		time.Sleep(time.Second)
 
 	}
